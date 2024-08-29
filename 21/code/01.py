fd287095b4ddc3b6408d734c42cb218884d06317
Fabian Peters <fabian.peters.ldf@gmail.com>
2024-08-29 23:43:29 +0200

    completed 2021-01

--- 21/code/01.py
+++ 21/code/01.py
@@ -3,10 +3,13 @@ import utilities as util
 def main():
     data = util.get_as_int_list()
     task_1(data)
-    
+    task_2(data)
 
 def task_1(data):
     print(sum([data[i] > data[i-1] for i in range(1, len(data))]))
+
+def task_2(data):
+    print(sum([sum(data[i:i+3]) > sum(data[i-1:i+2]) for i in range(1, len(data)-2)]))
 
 if __name__ == "__main__":
     main()

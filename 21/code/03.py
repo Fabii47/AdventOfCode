b62710667db2f5878fc0135d3524355aefaf52d4
Fabian Peters <fabian.peters.ldf@gmail.com>
2024-09-13 20:05:53 +0200

    started 21_03

---
+++ 21/code/03.py
@@ -0,0 +1,16 @@
+import utilities as util
+
+def main():
+    data = util.get_as_str_list("s")
+    task1(data)
+    task2(data)
+
+def task1(data):
+    gamma = []
+    pass
+
+def task2(data):
+    pass
+
+if __name__ == "__main__":
+    main()
